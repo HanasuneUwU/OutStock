c499862a4c7e2374142d1e7dfeeec347c13c2ed5
HanasuneUwU <161553331+HanasuneUwU@users.noreply.github.com>
2024-02-28 19:43:59 +0700

    Add files via upload

---
+++ ของหมด.js
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+	
+	button{
+		padding: 20px 50px;
+		font-size: 1.5rem;
+		cursor: pointer;
+		border: 0px;
+		background: transparent;
+		position: relative;
+		margin: 20px;
+		transition: all 0.25s ease;
+		align:center;
+	}
+.btn-7{
+	color:#fff;
+	overflow:hidden;
+	border-redius: 30px;
+	box-shadow: 0px -0px 0px 0px rgba(143,64,248, 0.5),
+	0px 0px 0px 0px rgba(39, 200, 255, 0.5);}
+	.btn-7:hover{
+		transform:translate(0, -6px);
+		box-shadow: 10px -10px 25px 0px rgba(143, 64, 248, 0.5)
+		, -10px 10px 25px 0px rgba(39,200,255, 0.5);
+		}
+		.btn-7:hover::after{
+			transform:rotate(150deg);}
+			.btn-7::after{
+				content:"";
+				width:400px;
+				height: 400px;
+				position:absolute;
+				top: -50px;
+				left: -100px;
+				background-color:#ff3cac;
+				background-image: linear-gradient(225deg,#27d86c 0%,#26caf8 50%,#c625d0 100%);
+				z-index:-1;
+				transition:all 0.25s ease;
+			 }
+	</style>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<a href="https://hanasuneuwu.github.io/SLIM-FIT-SHIRT/"><button class="btn-7">	
+              Home 
+              </button>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<p style="font-size: 25px;text-align:center ;">ขณะนี้สินค้าหมดเรากำลังจัดเติมสต็อกขออภัยในความไม่สะดวก</p>
+	
+
+</body>
+</html>
